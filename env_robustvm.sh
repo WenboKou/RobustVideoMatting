da9d450be56a5bf41a05511aaedb52ed5470b029
Wenbo Kou <kouwenbo09@gmail.com>
2022-02-16 16:09:15 +0800

    created env_robustvm.sh

---
+++ env_robustvm.sh
@@ -0,0 +1,7 @@
+pip install -i https://pypi.douban.com/simple av==8.0.3
+pip install -i https://pypi.douban.com/simple pims==0.5
+pip install -i https://pypi.douban.com/simple easing_functions==1.0.4
+pip install -i https://pypi.douban.com/simple tensorboard==2.5.0
+pip install -i https://pypi.douban.com/simple torch==1.9.0
+pip install -i https://pypi.douban.com/simple torchvision==0.10.0
+pip install -i https://pypi.douban.com/simple tqdm==4.61.1
